62640b34aa268972529f9f45eba4189c03c044f8
caishaoping <root@p-suoyse8wkea2-workstation.c.instruqt-prod.internal>
2021-01-22 21:41:20 +0000

    adding remote backend

---
+++ remote_backend.tf
@@ -0,0 +1,14 @@
+# If you are in a workshop...
+# Do not delete this file!
+# It's required to complete the Instruqt labs.
+terraform {
+backend "remote" {
+organization = "csp-training"
+
+workspaces {
+name = "hashicat-aws"
+}
+}
+}
+
+
